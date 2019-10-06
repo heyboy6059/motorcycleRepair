74e6baae476041f45ef524ae3702f7ae0ffea70a
Jee <yoojw94@gmail.com>
2019-10-07 01:32:11 +1000

    Add motorcycle page

---
+++ src/components/motorcycle.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Motorcycle extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Motorcycle Information</h1>
+      </div>
+
+    )
+  }
+}
+
+export default Motorcycle;
