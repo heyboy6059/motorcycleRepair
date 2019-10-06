1f0ceeb315d91a914406b875d764d2b9f0737bea
Jee <yoojw94@gmail.com>
2019-10-07 01:28:32 +1000

    Duplication

--- src/components/mechanic.js
+++ src/components/mechanic.js
@@ -5,23 +5,7 @@ class Mechanic extends Component {
     return(
       <div>
         <h1>Mechanic Registration</h1>
-        <div className="input-container">
-            <label htmlFor="username">Username</label>
-            <input type="text" placeholder="Enter Username" name="username" required>
-            </input>
-        </div>
-
-        <br></br>
-
-        <div className="input-container">
-            <label htmlFor="password">Password</label>
-            <input type="text" placeholder="Enter Password" name="password" required>
-            </input>
-        </div>
-
-        <br></br>
-
-        <button type="submit">SIGN IN</button>
+        
       </div>
 
     )
