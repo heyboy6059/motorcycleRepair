c1867b8f4814e91275f347e49c753fead16c650f
Daniel <heyboy6059@gmail.com>
2019-10-07 23:23:11 +1000

    initialised matching component for further development

---
+++ src/components/matching.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Matching extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Matching Page</h2>
+      </div>
+    );
+  }
+}
+
+export default Matching;
