a4a37d1c17012470340ba96d547c54c6e2fa438d
Daniel <heyboy6059@gmail.com>
2019-10-07 23:22:55 +1000

    added matching in switch/route in main component

--- src/components/main.js
+++ src/components/main.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import Mechanic from './mechanic';
 import MechaniRegistration from './mechanicRegistration';
 import Homepage from './homepage';
+import Matching from './matching';
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={Homepage} />
     {/* <Route path="/mechanic" component={Mechanic} /> */}
     <Route path="/mechanic" component={MechaniRegistration} />
+    <Route path="/matching" component={Matching} />
   </Switch>
 );
 
