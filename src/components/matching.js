674946b739cfc0dfb969bcda72f14062f83cd1ef
Jee <yoojw94@gmail.com>
2019-10-11 13:57:55 +1000

    new design style

--- src/components/matching.js
+++ src/components/matching.js
@@ -10,86 +10,88 @@ class Matching extends Component {
 
   render() {
     return (
-      <div className="container">
-        <h2>Matchings Page</h2>
-        {this.props.location.state.matchedData.map(mechanic => (
-          <div class="card" style={{ marginBottom: '2em' }}>
-            <h5 class="card-header list-title" style={{ color: '#f64f59' }}>
-              <img
-                src={require('../img/fast-forward.png')}
-                style={{ width: '2%', marginRight: '0.5em' }}
-              />
-              {mechanic.company}
-            </h5>
-            <div class="card-body">
-              <h5 class="card-title list-title">{mechanic.mechName}</h5>
-              <p class="card-text">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-sm">
-                      <span className="list-font">Licence# </span>
-                      <span className="list-result-font">
-                        {mechanic.licence}
-                      </span>
+      <div className="center-container">
+        <div className="container">
+          <h2>Matching Page</h2>
+          {this.props.location.state.matchedData.map(mechanic => (
+            <div class="card" style={{ marginBottom: '2em' }}>
+              <h5 class="card-header list-title" style={{ color: '#f64f59' }}>
+                <img
+                  src={require('../img/fast-forward.png')}
+                  style={{ width: '2%', marginRight: '0.5em' }}
+                />
+                {mechanic.company}
+              </h5>
+              <div class="card-body">
+                <h5 class="card-title list-title">{mechanic.mechName}</h5>
+                <p class="card-text">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-sm">
+                        <span className="list-font">Licence# </span>
+                        <span className="list-result-font">
+                          {mechanic.licence}
+                        </span>
+                      </div>
+                      <div class="col-sm">
+                        <span className="list-font">Phone </span>
+                        <span className="list-result-font">
+                          {mechanic.phoneNum}
+                        </span>
+                      </div>
                     </div>
-                    <div class="col-sm">
-                      <span className="list-font">Phone </span>
-                      <span className="list-result-font">
-                        {mechanic.phoneNum}
-                      </span>
+                    <div class="row">
+                      <div class="col-sm">
+                        <span className="list-font">Email </span>
+                        <span className="list-result-font">{mechanic.email}</span>
+                      </div>
+                      <div class="col-sm">
+                        <span className="list-font">Available Days </span>
+                        <span className="list-result-font">
+                          {mechanic.availableDays}
+                        </span>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-sm">
+                        <span className="list-font">Trading Hours </span>
+                        <span className="list-result-font">
+                          From
+                          {mechanic.wkHrStart} to
+                          {mechanic.wkHrEnd}
+                        </span>
+                      </div>
+                      <div class="col-sm">
+                        <span className="list-font">Rating </span>
+                        <span className="badge badge-pill badge-warning list-result-font">
+                          {mechanic.rating}
+                        </span>
+                      </div>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-sm">
-                      <span className="list-font">Email </span>
-                      <span className="list-result-font">{mechanic.email}</span>
-                    </div>
-                    <div class="col-sm">
-                      <span className="list-font">Available Days </span>
-                      <span className="list-result-font">
-                        {mechanic.availableDays}
-                      </span>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm">
-                      <span className="list-font">Trading Hours </span>
-                      <span className="list-result-font">
-                        From
-                        {mechanic.wkHrStart} to
-                        {mechanic.wkHrEnd}
-                      </span>
-                    </div>
-                    <div class="col-sm">
-                      <span className="list-font">Rating </span>
-                      <span className="badge badge-pill badge-warning list-result-font">
-                        {mechanic.rating}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </p>
-              <a
-                // href={'mailto:' + mechanic.email}
-                href={'tel:' + mechanic.phoneNum}
-                target="_top"
-                class="btn btn-lg btn-primary"
-              >
-                Book with phone
-              </a>
-              <a
-                style={{ marginLeft: '1.5em' }}
-                href={'mailto:' + mechanic.email}
-                target="_top"
-                class="btn btn-lg btn-primary"
-              >
-                Book with email
-              </a>
+                </p>
+                <a
+                  // href={'mailto:' + mechanic.email}
+                  href={'tel:' + mechanic.phoneNum}
+                  target="_top"
+                  class="btn btn-lg btn-primary"
+                >
+                  Book with phone
+                </a>
+                <a
+                  style={{ marginLeft: '1.5em' }}
+                  href={'mailto:' + mechanic.email}
+                  target="_top"
+                  class="btn btn-lg btn-primary"
+                >
+                  Book with email
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
 
-        {/* <MechInfoList data={this.state.information} /> */}
+          {/* <MechInfoList data={this.state.information} /> */}
+        </div>
       </div>
     );
   }
--- src/components/userServiceRequest.js
+++ src/components/userServiceRequest.js
@@ -111,159 +111,163 @@ class userServiceRequest extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <h2>Service Request</h2>
-        <form>
-          <div className="form-group row">
-            <label htmlFor="inputAddress" className="col-sm-2 col-form-label">
-              Address
-            </label>
-            <div className="col-sm-4">
-              <input
-                type="text"
-                className="form-control"
-                id="inputAddress"
-                placeholder="e.g. 1 main st, St Lucia QLD 4067"
-                onChange={this.addressChange}
-              />
+      <div className="center-container">
+        <div className="container">
+          <h2>Service Request</h2>
+          <form>
+            <div className="form-group row">
+              <label htmlFor="inputAddress" className="col-sm-2 col-form-label">
+                Address
+              </label>
+              <div className="col-sm-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputAddress"
+                  placeholder="e.g. 1 main st, St Lucia QLD 4067"
+                  onChange={this.addressChange}
+                />
+              </div>
             </div>
-          </div>
-          <div className="form-group row">
-            <label htmlFor="inputMake" className="col-sm-2 col-form-label">
-              Make
-            </label>
-            <div className="col-sm-4">
-              <input
-                type="text"
-                className="form-control"
-                id="inputMake"
-                placeholder="e.g. Honda"
-                onChange={this.makeChange}
-              />
+            <div className="form-group row">
+              <label htmlFor="inputMake" className="col-sm-2 col-form-label">
+                Make
+              </label>
+              <div className="col-sm-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputMake"
+                  placeholder="e.g. Honda"
+                  onChange={this.makeChange}
+                />
+              </div>
             </div>
-          </div>
-          <div className="form-group row">
-            <label htmlFor="inputModel" className="col-sm-2 col-form-label">
-              Model
-            </label>
-            <div className="col-sm-4">
-              <input
-                type="text"
-                className="form-control"
-                id="inputModel"
-                placeholder="e.g. CB300R"
-                onChange={this.modelChange}
-              />
+            <div className="form-group row">
+              <label htmlFor="inputModel" className="col-sm-2 col-form-label">
+                Model
+              </label>
+              <div className="col-sm-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputModel"
+                  placeholder="e.g. CB300R"
+                  onChange={this.modelChange}
+                />
+              </div>
             </div>
-          </div>
-          <div className="form-group row">
-            <label htmlFor="inputYear" className="col-sm-2 col-form-label">
-              Year
-            </label>
-            <div className="col-sm-4">
-              <input
-                type="text"
-                className="form-control"
-                id="inputYear"
-                placeholder="e.g. 2018"
-                onChange={this.yearChange}
-              />
+            <div className="form-group row">
+              <label htmlFor="inputYear" className="col-sm-2 col-form-label">
+                Year
+              </label>
+              <div className="col-sm-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputYear"
+                  placeholder="e.g. 2018"
+                  onChange={this.yearChange}
+                />
+              </div>
             </div>
-          </div>
-          <div className="form-group row">
-            <label
-              className="col-sm-2 col-form-label"
-              htmlFor="inputServiceType"
-            >
-              Service Type
-            </label>
-            <select
-              className="custom-select col-sm-3"
-              id="inputServiceType"
-              onChange={this.serviceTypeChange}
-              style={{ marginLeft: '1em' }}
-            >
-              <option value="0">Choose...</option>
-              <option value="Full">Full</option>
-              <option value="Minor">Minor</option>
-              <option value="Oil Check">Oil Check</option>
-              <option value="Other">Other (add notes)</option>
-            </select>
-          </div>
+            <div className="form-group row">
+              <label
+                className="col-sm-2 col-form-label"
+                htmlFor="inputServiceType"
+              >
+                Service Type
+              </label>
+              <select
+                className="custom-select col-sm-3"
+                id="inputServiceType"
+                onChange={this.serviceTypeChange}
+                style={{ marginLeft: '1em' }}
+              >
+                <option value="0">Choose...</option>
+                <option value="Full">Full</option>
+                <option value="Minor">Minor</option>
+                <option value="Oil Check">Oil Check</option>
+                <option value="Other">Other (add notes)</option>
+              </select>
+            </div>
 
-          <div className="form-group row">
-            <label
-              className="col-sm-2 col-form-label"
-              htmlFor="inputServiceDay"
-            >
-              Service Day
-            </label>
-            <select
-              className="custom-select col-sm-3"
-              id="inputServiceDay"
-              onChange={this.serviceDayChange}
-              style={{ marginLeft: '1em' }}
-            >
-              <option value="0">Choose...</option>
-              <option value="Weekdays">Weekdays</option>
-              <option value="Weekends">Weekends</option>
-              <option value="Mon-Thurs">Mon-Thurs</option>
-              <option value="Mon-Sat">Mon-Sat</option>
-              <option value="Wed-Sun">Wed-Sun</option>
-            </select>
-          </div>
-
-          <div className="form-group row">
-            <label
-              className="col-sm-2 col-form-label"
-              htmlFor="inputServiceFrom"
-            >
-              Service Time
-            </label>
-            <div className="col-sm-2">
-              <input
-                type="text"
-                className="form-control"
-                id="inputServiceTimeFrom"
-                placeholder="From"
-                onChange={this.serviceFromChange}
-              />
+            <div className="form-group row">
+              <label
+                className="col-sm-2 col-form-label"
+                htmlFor="inputServiceDay"
+              >
+                Service Day
+              </label>
+              <select
+                className="custom-select col-sm-3"
+                id="inputServiceDay"
+                onChange={this.serviceDayChange}
+                style={{ marginLeft: '1em' }}
+              >
+                <option value="0">Choose...</option>
+                <option value="Weekdays">Weekdays</option>
+                <option value="Weekends">Weekends</option>
+                <option value="Mon-Thurs">Mon-Thurs</option>
+                <option value="Mon-Sat">Mon-Sat</option>
+                <option value="Wed-Sun">Wed-Sun</option>
+              </select>
             </div>
-            <div className="col-sm-2">
-              <input
-                type="text"
-                className="form-control"
-                id="inputserviceTo"
-                placeholder="To"
-                onChange={this.serviceToChange}
-              />
-            </div>
-          </div>
 
-          <div className="form-group row">
-            <label htmlFor="inputNotes" className="col-sm-2 col-form-label">
-              Notes
-            </label>
-            <div className="col-sm-4">
-              <textarea
-                className="form-control"
-                id="inputNotes"
-                placeholder=""
-                onChange={this.notesChange}
-              ></textarea>
+            <div className="form-group row">
+              <label
+                className="col-sm-2 col-form-label"
+                htmlFor="inputServiceFrom"
+              >
+                Service Time
+              </label>
+              <div className="col-sm-2">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputServiceTimeFrom"
+                  placeholder="From"
+                  onChange={this.serviceFromChange}
+                />
+              </div>
+              <div className="col-sm-2">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputserviceTo"
+                  placeholder="To"
+                  onChange={this.serviceToChange}
+                />
+              </div>
             </div>
-          </div>
 
-          <div className="form-group row">
-            <button
-              type="submit"
-              className="btn btn-primary btn-lg my-1"
-              onClick={this.handleSubmit}
-            >
-              Request
-            </button>
-          </div>
-        </form>
+            <div className="form-group row">
+              <label htmlFor="inputNotes" className="col-sm-2 col-form-label">
+                Notes
+              </label>
+              <div className="col-sm-4">
+                <textarea
+                  className="form-control"
+                  id="inputNotes"
+                  placeholder=""
+                  onChange={this.notesChange}
+                ></textarea>
+              </div>
+            </div>
+
+            <div className="form-group row">
+              <div className="btn2">
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-lg my-1"
+                  onClick={this.handleSubmit}
+                >
+                  Request
+                </button>
+              </div>  
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
